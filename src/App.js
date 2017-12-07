2f496213be89ea13751225a256e661be3a4b41d1
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2017-12-07 17:41:07 +0300

    add fetching ip and weather

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { HashRouter as Router,
          Route,
          Switch,
@@ -6,85 +6,66 @@ import { HashRouter as Router,
 } from 'react-router-dom';
 import './App.css';
 
-const data = [
-  {
-    id: 0,
-    title: "список покупок",
-    todos: ["молоко", "творожок"]
-
-  },
-  {
-    id: 1,
-    title: "список домашних дел",
-    todos: ["почистить картошку", "полежать на диване"]
-  },
-  {
-    id: 2,
-    title: "список домашних дел",
-    todos: ["почистить картошку", "полежать на диване"]
+class App extends Component {
+  state = {
+    city: 'Saint Petersburg',
+    temperature: '',
+    fetching: true,
   }
-];
 
-const NotFound = () => (
-  <div>
-    <h2>Not found 404</h2>
-    <Link to="/">На главную</Link>
-  </div>
-);
+  // когда компонент появится в DOM
+  componentDidMount() {
+    //this.fetchZipCodeByIP();
+    this.fetchWeatherData(this.state.city);
+  }
 
-const TodoLists = () => {
-  const listContent = data.map( (list) => (
-    <li key={list.id}>
-      <Link to={`/lists/${list.id}`}>{list.title}</Link>
-    </li>
-  ));
-  return (
-    <div>
-      <h2>List of all todoLists</h2>
-      <p>всего списков: {data.length}</p>
-      <ul>
-        {listContent}
-      </ul>
-    </div>
-  )
-};
+  render() {
+    return (
+      <div>
+        City: {this.state.city}
+        <br/>
+        Temperature: {this.state.temperature}
+      </div>
+    )
+  }
 
-const TodoList = (props) => {
-  const id = props.match.params.list;
-  console.log( data[id] );
-  const listOfTodos = data[id].todos.map( (item, key) => (
-    <li key={key}>
-      {item}
-    </li>
-  ));
-  return (
-    <div>
-      <Link to="/">Вернуться к cпискам:</Link>
-      <h2>{data[id].title}</h2>
-      {listOfTodos}
-    </div>
-  )
-};
+  // get city location by ip
+  fetchZipCodeByIP = () => {
+    fetch(`https://freegeoip.net/json/`)
+      //.then(response => response.json())
+      .then((response) => {
+        console.log('fetching ip from https://freegeoip.net');
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.fetchWeatherData( data.city );
+      })
+      .catch((error) => {
+        console.log(error)
+        return
+      })
+  };
 
-const Todo = () => (
-  <div>
-    <h1>название задачи</h1>
-    <Link to="/"/>
-  </div>
-);
+  // get weather data by zip code
+  fetchWeatherData = (city) => {
+    const baseUrl = `http://api.openweathermap.org`;
+    const path = `/data/2.5/weather`;
+    const appId = `a8ea301f97eff232520187299d334108`;
+    const query = `units=metric&lang=ru&appid=${appId}`;
 
+    fetch(`${baseUrl}${path}?q=${city}&${query}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          city: data.name,
+          temperature: Math.round(data.main.temp),
+          fetching: false
+        });
+      })
+      .catch(error => console.error(error));
+  };
 
-const App = () => (
-  <Router>
-    <div>
-      <Switch >
-        <Route exact path="/" component={TodoLists}/>
-        <Route path="/lists/:list" component={TodoList}/>
-        <Route path="/lists/:list/:todo" component={Todo}/>
-        <Route path="*" component={NotFound}/>
-      </Switch>
-    </div>
-  </Router>
-);
+}
 
 export default App;
