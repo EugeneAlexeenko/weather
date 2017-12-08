13c67e6c8377c37de0ef375611400f9147fe9995
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2017-12-08 13:17:20 +0300

    add store, reducers, thunk, devtools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk)
+));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
